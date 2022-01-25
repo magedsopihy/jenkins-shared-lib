a480775c40d3fd736455c2fcf9fe73ba02745563
magedsopihy <magedsopihy@gmail.com>
2022-01-25 19:38:03 +0200

    parameterize tha liberary

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -1,10 +1,10 @@
 #!/usr/bin/env groovy
 
-def call(){
+def call(String imageName){
     echo "building the docker image"
     withCredentials([usernamePassword(credentialsId: 'DOCKER_HUB', passwordVariable: 'PASS', usernameVariable: 'USERNAME')]){
-        sh 'docker build -t magedsopihy/maven_project:1.0 .'
-        sh 'echo $PASS | docker login -u $USERNAME --password-stdin'
-        sh 'docker push magedsopihy/maven_project:1.0'
+        sh "docker build -t $imageName ."
+        sh "echo $PASS | docker login -u $USERNAME --password-stdin"
+        sh "docker push $imageName"
     }
 }
